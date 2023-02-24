29c0d6e1ce7dcb9d3c7797bb8a9ee669f567df16
Shane Slattery <Slattz@users.noreply.github.com>
2023-02-24 18:36:28 +0000

    Add comments to FuncAnnotationsParser

--- main.cpp
+++ main.cpp
@@ -13,17 +13,17 @@ struct FuncAnnotationsParser : llvm::PassInfoMixin<FuncAnnotationsParser> {
 
     // Takes IR unit to run the pass on Module and the corresponding manager
     llvm::PreservedAnalyses run(llvm::Module& M, llvm::ModuleAnalysisManager&) {
-        llvm::GlobalVariable* globalAttrs = M.getGlobalVariable("llvm.global.annotations");
+        llvm::GlobalVariable* globalAttrs = M.getGlobalVariable("llvm.global.annotations"); //Get llvm.global.annotations from IR data
         if (globalAttrs) {
-            llvm::ConstantArray* array = llvm::cast<llvm::ConstantArray>(globalAttrs->getOperand(0));
-            for (llvm::Value* operand : array->operands()) {
-                if (llvm::ConstantStruct* constant = llvm::dyn_cast<llvm::ConstantStruct>(operand)) {
-                    if (constant->getNumOperands() >= 2) {
-                        if (llvm::Function* func = llvm::cast<llvm::Function>(constant->getOperand(AnnotationOperands::FUNCTION_OPERAND))) {
-                            if (llvm::GlobalVariable* globalStrPtr = llvm::cast<llvm::GlobalVariable>(constant->getOperand(AnnotationOperands::ANNOTATE_OPERAND))) {
-                                if (llvm::ConstantDataArray* strArray = llvm::dyn_cast<llvm::ConstantDataArray>(globalStrPtr->getOperand(0))) {
-                                    func->addFnAttr(strArray->getAsString());
-                                }
+            llvm::ConstantArray* array = llvm::cast<llvm::ConstantArray>(globalAttrs->getOperand(0)); //Get array of operands
+            for (llvm::Value* operand : array->operands()) { //Get each operand
+                if (llvm::ConstantStruct* constant = llvm::dyn_cast<llvm::ConstantStruct>(operand)) { //Cast operand to a struct (i.e. the annotation struct)
+                    if (constant->getNumOperands() >= 2) { //Must be at least two operands (FUNCTION_OPERAND and ANNOTATE_OPERAND)
+                        llvm::Function* func = llvm::cast<llvm::Function>(constant->getOperand(AnnotationOperands::FUNCTION_OPERAND)); //Get function
+                        llvm::GlobalVariable* globalStrPtr = llvm::cast<llvm::GlobalVariable>(constant->getOperand(AnnotationOperands::ANNOTATE_OPERAND));
+                        if (func && globalStrPtr) {
+                            if (llvm::ConstantDataArray* strArray = llvm::dyn_cast<llvm::ConstantDataArray>(globalStrPtr->getOperand(0))) { //Get Annotation str
+                                func->addFnAttr(strArray->getAsString()); //add Annotation to function
                             }
                         }
                     }
