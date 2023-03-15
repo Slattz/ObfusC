b97667ec04116c66c03b65686e102b0a4001cb35
Shane Slattery <Slattz@users.noreply.github.com>
2023-03-15 12:19:04 +0000

    MbaPass: Add optimisations and comments

--- source/MBA/MbaPass.cpp
+++ source/MBA/MbaPass.cpp
@@ -4,10 +4,10 @@
 
 namespace obfusc {
     MbaPass::MbaPass() {
-        std::random_device rd;
-        m_randGen64.seed(rd());
+        std::random_device rd;  //Get hardware specific random device
+        m_randGen64.seed(rd()); //Seed Mersenne Twister random generator  
     }
-    
+
     MbaPass::~MbaPass() {}
 
     bool MbaPass::obfuscate(llvm::Module& mod, llvm::Function& func) {
@@ -15,22 +15,22 @@ namespace obfusc {
 
         for (auto& block : func) {
             for (auto instruction = block.begin(); instruction != block.end(); instruction++) {
-                llvm::BinaryOperator* binOp = llvm::dyn_cast<llvm::BinaryOperator>(instruction);
-                if (!binOp)
+                llvm::BinaryOperator* binOp = llvm::dyn_cast<llvm::BinaryOperator>(instruction); 
+                if (!binOp) //Is instruction a binary op
                     continue;
 
                 llvm::IRBuilder irBuilder(binOp);
-                llvm::Type* newType = llvm::IntegerType::getInt128Ty(binOp->getType()->getContext());
+                llvm::Type* newType = llvm::IntegerType::getInt128Ty(binOp->getType()->getContext()); //Set type to 128 bit to account for possible int overflows
 
                 llvm::Value* op1 = GenStackAlignmentCode(irBuilder, newType, binOp->getOperand(0));
                 llvm::Value* op2 = GenStackAlignmentCode(irBuilder, newType, binOp->getOperand(1));
 
-                op1 = Substitute(irBuilder, newType, op1);
-                op2 = Substitute(irBuilder, newType, op2);
+                op1 = Substitute(irBuilder, newType, op1); //Generate MBA for op1
+                op2 = Substitute(irBuilder, newType, op2); //Generate MBA for op2
 
                 llvm::Instruction* newInstrs;
                 unsigned opCode = binOp->getOpcode();
-                switch (opCode) {
+                switch (opCode) { //Gen underlying operation
                 case llvm::Instruction::Add:
                     newInstrs = llvm::BinaryOperator::CreateAdd(op1, op2);
                     break;
@@ -75,14 +75,17 @@ namespace obfusc {
                     continue;
                 }
 
-                llvm::ReplaceInstWithInst(block.getInstList(), instruction, newInstrs);
+                llvm::ReplaceInstWithInst(block.getInstList(), instruction, newInstrs); //replace old instruction with new obfuscation instructions
                 changed = true;
             }
+
+            llvm::outs() << "Function " << func.getName() << " Has Been Obfuscated: " << changed << "\n";
         }
 
         return changed;
     }
 
+    /* Get a random number that is half the types bit length (e.g. 64 bit for 128 bit values) */
     int MbaPass::GetRandomNumber(llvm::Type* type) {
         uint64_t modNum = UCHAR_MAX; 
         if (type->getIntegerBitWidth() == 16) {
@@ -98,6 +101,7 @@ namespace obfusc {
         return m_randGen64()%modNum;
     }
 
+    /* Gen loads and stores to convert initial value to a 128 bit value (e.g. 32 bit to 128 bit) */
     llvm::Value* MbaPass::GenStackAlignmentCode(llvm::IRBuilder<>& irBuilder, llvm::Type* newType, llvm::Value* operand) {
         llvm::Instruction* alloc = irBuilder.CreateAlloca(newType);
         auto constZero = llvm::ConstantInt::get(newType, 0);
@@ -106,8 +110,8 @@ namespace obfusc {
         return irBuilder.CreateLoad(newType, alloc);
     }
 
+    /* Recursive function to generate the various mixed arithmetic IR. Essentially creates a reversible tree of IR instructions */
     llvm::Value* MbaPass::Substitute(llvm::IRBuilder<>& irBuilder, llvm::Type* type, llvm::Value* operand, size_t numRecursions) {
-        llvm::outs() << "Op: " << numRecursions << "\t" << *operand << "\n";
         if (numRecursions >= s_RecursiveAmount) {
             return operand;
         }
@@ -116,40 +120,41 @@ namespace obfusc {
         if (instr) {
             unsigned int opCode = instr->getOpcode();
             if ((opCode == llvm::Instruction::Add) || (opCode == llvm::Instruction::Sub) || (opCode == llvm::Instruction::UDiv) ||
-                (opCode == llvm::Instruction::SDiv) || (opCode == llvm::Instruction::Xor)
-            ) {
+                (opCode == llvm::Instruction::SDiv) || (opCode == llvm::Instruction::Xor)) 
+            {
                 operand->mutateType(type);
             }
         }
 
-        int randType = rand() % SubstituteType::Max;
-        uint64_t randNum = GetRandomNumber(type);
+        int randType = rand() % SubstituteType::Max; //Randomly make obfuscation operation
+        auto randVal = llvm::ConstantInt::get(type, GetRandomNumber(type)); //Get random number for each operation
 
+        /* x = (x - randVal) + randVal */
         if (randType == SubstituteType::Add) {
-            auto randVal = llvm::ConstantInt::get(type, randNum);
             auto a = Substitute(irBuilder, type, irBuilder.CreateSub(operand, randVal), numRecursions+1);
             return irBuilder.CreateAdd(a, randVal);
         }
 
+        /* x = (x + randVal) - randVal */
         else if (randType == SubstituteType::Subtract) {
-            auto randVal = llvm::ConstantInt::get(type, randNum);
             auto a = Substitute(irBuilder, type, irBuilder.CreateAdd(operand, randVal), numRecursions+1);
             return irBuilder.CreateSub(a, randVal);
         }
 
+        /* x = (x * randVal) / randVal */
         else if (randType == SubstituteType::Divide) {
-            auto randVal = llvm::ConstantInt::get(type, randNum);
             auto a = Substitute(irBuilder, type, irBuilder.CreateMul(operand, randVal), numRecursions+1);
             return irBuilder.CreateSDiv(a, randVal);
         }
 
+        /* x = ~(~x) */
         else if (randType == SubstituteType::Not) {
             auto a = Substitute(irBuilder, type, irBuilder.CreateNot(operand), numRecursions+1);
             return irBuilder.CreateNot(a);
         }
 
+        /* x = (x ^ randVal) ^ randVal */
         else if (randType == SubstituteType::Xor) {
-            auto randVal = llvm::ConstantInt::get(type, randNum);
             auto a = Substitute(irBuilder, type, irBuilder.CreateXor(operand, randVal), numRecursions+1);
             return irBuilder.CreateXor(a, randVal);
         }
